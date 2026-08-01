2fb69c35ee694b7758afcbcb6e2ebb7a1f8018cd
Vladimir Mazhaev <jetiaks@gmail.com>
2026-08-01 10:34:23 +0500

    Add common logging functions

--- modules/core/homebrew/homebrew.sh
+++ modules/core/homebrew/homebrew.sh
@@ -2,10 +2,10 @@ check_homebrew() {
 
     info "Проверяю Homebrew..."
 
-    if command -v brew >/dev/null; then
-        info "Homebrew уже установлен"
+    if command -v brew >/dev/null 2>&1; then
+        success "Homebrew уже установлен"
     else
-        info "Homebrew не найден"
+        warning "Homebrew не найден"
     fi
 
 }
