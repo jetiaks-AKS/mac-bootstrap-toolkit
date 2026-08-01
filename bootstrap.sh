c34ed3ebf6771adda2d829c2a74204f0dcda598e
Vladimir Mazhaev <jetiaks@gmail.com>
2026-08-01 12:04:48 +0500

    Add Git module

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,11 @@
 
 source modules/core/common/common.sh
 source modules/core/homebrew/homebrew.sh
+source modules/core/git/git.sh
 
 info "Mac Bootstrap Toolkit"
 
 run_module "Homebrew" check_homebrew
+run_module "Git" check_git
 
 show_summary
